d3ff177fc302d44ff39f36218d7d9887fe9cf3bc
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-08 21:20:40 +0800

    Add random number at game start

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -3,12 +3,18 @@ package com.oocl;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class GuessNumberTest {
+public class GuessNumberTest  {
     @Test
     public void should_output_xAxB_for_normal_user_input() {
         GuessNumber guessNumber = new GuessNumber();
         guessNumber.setUserInput("1245");
         guessNumber.setSecretNumber("1234");
         Assert.assertEquals("2A1B", guessNumber.play());
+    }
+
+    @Test
+    public void should_output_xAxB_message_for_random_number() {
+        GuessNumber guessNumber = new GuessNumber();
+        Assert.assertEquals("Random number is generated. Game Start!", guessNumber.initial());
     }
 }
